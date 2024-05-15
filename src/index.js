4be74a4bc3d09b4ad0d9b2b758c310136e49cfdd
gloriacullelldeveloper <gcullelldev@gmail.com>
2024-05-15 20:28:53 +0200

    Added JS file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function searchCity(city) {
+
+
+    let apiKey = "c71c7bae8f4otf0ad66fa62c747c8831";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}';
+
+
+}
+
+
+
+
+function handleSearchSubmit(event) {
+
+
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+ 
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
